2190c2b9253dfbfdd86e402c25c4a462e0f4248b
amir2015 <zahranovich2@gmail.com>
2025-07-21 15:21:59 +0300

    hotels-controller

--- src/hotels/hotels.module.ts
+++ src/hotels/hotels.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { Hotel } from './entities/hotel.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HotelsController } from './hotels.controller';
+import { HotelsService } from './hotels.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Hotel])],
+  controllers: [HotelsController],
+  providers: [HotelsService],
+  exports: [HotelsService],
+})
 export class HotelsModule {}
